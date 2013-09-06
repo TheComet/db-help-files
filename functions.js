835febc48b7bc8923071cae23ac53eb18c27539c
Neil Moore <dar13.moore@gmail.com>
2013-09-06 15:01:30 -0400

    Created new Javascript function to replace 'toggleVisibility' as it was ugly. 'toggleVisibility2' takes the same arguments but assumes that the corresponding content div has an id of form '<link-id>-content'. Also, instead of 'name=<nameofelement>' use id as that makes it easier to pull things out of the DOM.

--- functions.js
+++ functions.js
@@ -1,19 +1,14 @@
-function toggleVisibility(thisCommand)
+function toggleVisibility2(thisCommand)
 {
-    //Find the command's content entity (In a retarded way)
-    b = thisCommand.nextSibling; //THIS
-    b = b.nextSibling;           //IS
-    b = b.nextSibling;           //CRAP!!
-
-    if(thisCommand.className == "commandUnfolded")
-    {
-	thisCommand.className = "commandFolded";
-	b.style.display = "none";
-    }
-    else
-    {
-	thisCommand.className = "commandUnfolded";
-	b.style.display = "block";
-    }
-    return;
+	var b = document.getElementById(thisCommand.id + "-content");
+	if(thisCommand.className == "commandUnfolded")
+	{
+		thisCommand.className = "commandFolded";
+		b.style.display = "none";
+	}
+	else
+	{
+		thisCommand.className = "commandUnfolded";
+		b.style.display = "block";
+	}
 }
